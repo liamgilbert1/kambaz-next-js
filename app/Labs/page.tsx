d9f1c4cf85655f1a0d84445b1a4ae9cf9d1cc5b9
Liam Gilbert <gilbert.li@northeastern.edu>
2025-09-17 17:32:59 -0400

    added class section

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -4,6 +4,7 @@ export default function Labs() {
     <div id="wd-labs">
       <h1>Labs</h1>
       <p>Liam Gilbert</p>
+      <p>Section: CS4550.11597.202610</p>
       <ul>
         <li>
           <Link href="/Labs/Lab1" id="wd-lab1-link">
