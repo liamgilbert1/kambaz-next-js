31ad475a446015247c04187362de2d3a6b61b7a9
Liam Gilbert <gilbert.li@northeastern.edu>
2025-09-17 16:57:54 -0400

    added first and last name to Labs/page.tsx

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,6 +3,7 @@ export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
+      <p>Liam Gilbert</p>
       <ul>
         <li>
           <Link href="/Labs/Lab1" id="wd-lab1-link">
