586aa5e2ae493615e4c1ff001a88795031c020f4
Liam Gilbert <gilbert.li@northeastern.edu>
2025-09-18 13:59:12 -0400

    Added another module

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -19,6 +19,17 @@ export default function Modules() {
         </li>
         <li className="wd-module">
           <div className="wd-title">Week 2</div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Styling Web Pages with CSS and Bootstrap, Assignment 2
+                </li>
+                <li className="wd-content-item">CSS Basics</li>
+              </ul>
+            </li>
+          </ul>
         </li>
         <li className="wd-module">
           <div className="wd-title">Week 3</div>
