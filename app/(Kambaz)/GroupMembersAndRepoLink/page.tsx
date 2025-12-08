15c06852d86f29be84a39b4e194c6670881b74a0
Joe <carlucci.j@northeastern.edu>
2025-12-07 21:17:08 -0500

    Page with group members and repo links added to nav bar

---
+++ app/(Kambaz)/GroupMembersAndRepoLink/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+export default function Lab1() {
+    return (
+      <div id="wd-lab1">
+        <h1>Group Project Information</h1>
+        <br />
+        <h4>Joseph Carlucci, Section 01</h4>
+        <h4>Liam Gilbert, Section 01</h4>
+        <br />
+        <h4>GitHub Repository Links:</h4>
+        <a href="https://github.com/liamgilbert1/kambaz-next-js" target="_blank">
+          Front End Repo
+        </a>
+        <br />
+        <h4>Back End GitHub Repository Link:</h4>
+        <a href="https://github.com/liamgilbert1/kambaz-node-server-app" target="_blank">
+          Back End Repo
+        </a>
+      </div>
+    );
+}
--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -2,7 +2,7 @@
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
-import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { FaInbox, FaRegCircleUser, FaUsers } from "react-icons/fa6";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
@@ -14,6 +14,7 @@ export default function KambazNavigation() {
     { label: "Calendar", path: "/Calendar", icon: IoCalendarOutline },
     { label: "Inbox", path: "/Inbox", icon: FaInbox },
     { label: "Labs", path: "/Labs", icon: LiaCogSolid },
+    { label: "Group Members", path: "/GroupMembersAndRepoLink", icon: FaUsers },
   ];
   return (
     <ListGroup
